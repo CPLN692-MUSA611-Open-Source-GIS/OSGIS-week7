42829d7346100c952dbd8b446a37e734481ee93f
Tyler Bradford <32780422+tybradf@users.noreply.github.com>
2021-03-23 14:57:57 -0400

    Update main.js
    Thru task 6

--- lab/js/main.js
+++ lab/js/main.js
@@ -126,12 +126,16 @@ of the application to report/display this information.
 
 ===================== */
 
-var dataset = ""
+var dataset = "https://raw.githubusercontent.com/CPLN692-MUSA611-Open-Source-GIS/datasets/master/geojson/philadelphia-garbage-collection-boundaries.geojson"
 var featureGroup;
 
 var myStyle = function(feature) {
-  return {};
-};
+      if (feature.properties.COLLDAY === 'MON') {return {color: '#DFFF00'}}
+      else if (feature.properties.COLLDAY === 'TUE') {return {color: '#0400ff'}}
+      else if (feature.properties.COLLDAY === 'WED') {return {color: '#e29607'}}
+      else if (feature.properties.COLLDAY === 'THU') {return {color: '#DE3163'}}
+      else if (feature.properties.COLLDAY === 'FRI') {return {color: '#9FE2BF'}}
+  };
 
 var showResults = function() {
   /* =====================
@@ -155,12 +159,28 @@ var eachFeatureFunction = function(layer) {
     you can use in your application.
     ===================== */
     console.log(layer.feature);
+    let dowDisplay = ''
+    if (layer.feature.properties.COLLDAY === 'MON') {dowDisplay = 'Monday'}
+    else if (layer.feature.properties.COLLDAY === 'TUE') {dowDisplay = 'Tuesday'}
+    else if (layer.feature.properties.COLLDAY === 'WED') {dowDisplay = 'Wednesday'}
+    else if (layer.feature.properties.COLLDAY === 'THU') {dowDisplay = 'Thursday'}
+    else if (layer.feature.properties.COLLDAY === 'FRI') {dowDisplay = 'Friday'}
+    else if (layer.feature.properties.COLLDAY === 'SAT') {dowDisplay = 'Saturday'}
+    else if (layer.feature.properties.COLLDAY === 'SUN') {dowDisplay = 'Sunday'}
+
+    $('.day-of-week').text(dowDisplay)
+    var mapBounds = event.target.getBounds();
+    map.fitBounds(mapBounds);
     showResults();
   });
 };
 
 var myFilter = function(feature) {
-  return true;
+  if (feature.properties.COLLDAY != ' ' ) {
+    return true;
+  } else {
+    return false
+  }
 };
 
 $(document).ready(function() {
